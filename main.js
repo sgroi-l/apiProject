d4df7c02f6d54998a2ccfb4b340b2e5a65c3efab
Georgina Appeagyei <georginaappeagyei@Georginas-MBP.lan>
2023-06-11 08:50:48 +0100

    data render javascript
    Relates #7

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function getRandomItem(array) {
 
 const form = document.querySelector("#searchForm");
 const searchLink = document.querySelector('#searchLink'); // I replaced the submit button on the form with an icon. 
-
+const renderElement = document.querySelector('#data-render')
 
 
 searchLink.addEventListener("click", function(event) {
@@ -35,7 +35,16 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=62f3a1ae
 
     // Use the temperature value as needed
     console.log(`Temperature in ${data.name}, ${data.sys.country}: ${temperature}\u00B0`);
-
+    // Clear weather data
+    let weatherElement = "";
+    // Create element
+    weatherElement = document.createElement("h1");
+    weatherElement.setAttribute("id", "weather");
+    weatherText = document.createTextNode(`${data.name}, ${data.sys.country}: ${temperature}\u00B0C`)
+    // append elements 
+    renderElement.append(weatherElement);
+    weatherElement.append(weatherText);
+    console.log(renderElement);
     // Second promise
     let typeArray;
     if (temperature >= 20) {
@@ -56,6 +65,16 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=62f3a1ae
       .then((jsonData) => {
         console.log(jsonData);
         console.log(jsonData.type);
+        // Create element
+        activityElement = document.createElement('h2');
+        // Set ID attribute to activity
+        activityElement.setAttribute('id', "activity")
+        // Create text for activity element
+        activityText = document.createTextNode(`Try a ${jsonData.type} activity today. ${jsonData.activity}. You will need ${jsonData.participants} participant(s) and £${jsonData.price}.`);
+        // append elements
+        activityElement.append(activityText);
+        renderElement.append(activityElement);
+        console.log(renderElement);
       })
       .catch((error) => console.error(error));
   })
